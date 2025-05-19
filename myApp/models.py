4a5261a51d52b3f7d3e8604c19e655c21c8e794c
AsHkaN <ashkan_ahrari@yahoo.com>
2025-05-19 19:02:28 +0300

    polling system models added.

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Poll(models.Model):
+    title = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    body = models.CharField(max_length=150)
+    poll = models.ForeignKey(Poll, related_name='questions', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.body
+    
+
+class Answer(models.Model):
+    body = models.CharField(max_length=150)
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.body
+    
+
+class Response(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='user_surveys', on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, related_name='answer_surveys', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} chose {self.answer.body}"
