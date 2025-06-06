8969df193d092078282cadc93d0150eda0cf368b
AsHkaN <ashkan_ahrari@yahoo.com>
2025-06-06 18:37:19 +0300

    socket bugs fixed

--- myApp/routing.py
+++ myApp/routing.py
@@ -2,6 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    r"^ws/poll/(?P<poll_id>\d+)/$",         # captures poll_id from the URL
-    consumers.PollConsumer.as_asgi()         # the consumer that handles it
+    re_path(r'ws/poll/(?P<poll_id>\d+)/$', consumers.PollConsumer.as_asgi()),
 ]
